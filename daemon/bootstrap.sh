36aa2ddb9196661d4c74c9d2834e8d28f2e3d9a6
gobomb <bombcoder@outlook.com>
2020-10-16 18:25:37 +0800

    update bootstrap

--- daemon/bootstrap.sh
+++ daemon/bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker run -d --name="video-daemon" --restart=always --net=host --privileged \
+docker run -d --name="video-daemon-rpc" --restart=always --net=host --privileged \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v /root/.Xauthority:/root/.Xauthority \
 	-v /run/docker.sock:/run/docker.sock \
 	-v /var/lib/docker:/var/lib/docker:ro \
 	-e DISPLAY=":0.0" -e QT_X11_NO_MITSHM=1 -e QT_GRAPHICSSYSTEM="native" \
 	--device="/dev/video0:/dev/video0" \
-	registry.local/face_recognition_daemon:sixsq
+	registry.local/face_recognition_daemon:rpc
--- detector/bootstrap.sh
+++ detector/bootstrap.sh
@@ -3,4 +3,4 @@
 docker run -d --name=detector --net=host \
 	-e CAPTURE_INTERVAL="60"   -e FRONTEND_SVC="frontend_svc:8080" \
 	-e NODE_HOST="10.10.67.2" \
-	registry.local/face_recognition_detector:sixsq
+	registry.local/face_recognition_detector:rpc
